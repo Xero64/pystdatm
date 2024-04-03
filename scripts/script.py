b0b4027182182dbea1c0e89db1445acbe94cfe43
Xero64 <xero64@gmail.com>
2024-04-03 14:09:01 +0200

    Updates to script.py.

--- scripts/script.py
+++ scripts/script.py
@@ -1,13 +1,22 @@
 #%%
 # Import Dependencies
-from pystdatm import density, pressure
+from pystdatm import density, pressure, viscosity, speed_of_sound, temperature
 
 #%%
 # Sample Code
-alt = 1000.0 # metres above sea level
+alt = 0.0 # metres above sea level
 
 rho = density(alt)
-print(f'rho = {rho:.3f} kg/m**3')
+print(f'rho = {rho:} kg/m**3 at Altitude = {alt:} m\n')
+
+mu = viscosity(alt)
+print(f'mu = {mu:} Pa.s at Altitude = {alt:} m\n')
 
 p = pressure(alt)
-print(f'p = {p:.0f} Pa')
+print(f'p = {p:} Pa at Altitude = {alt:} m\n')
+
+a = speed_of_sound(alt)
+print(f'a = {a:} m/s at Altitude = {alt:} m\n')
+
+temp = temperature(alt)
+print(f'temp = {temp:} K at Altitude = {alt:} m\n')
